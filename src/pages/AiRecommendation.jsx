e187e3dfd8b6805cc1f9e647697534a167230994
Hillary Tanui <hillarytanui80@gmail.com>
2025-10-29 14:09:53 +0300

    added Airecommendation page

--- src/pages/AiRecommendation.jsx
+++ src/pages/AiRecommendation.jsx
@@ -47,8 +47,8 @@ import React, { useState } from "react";
  }, {})
  
 export default function AiRecommendation() {
-
-  const [inputs, setInputs] = useState(initialState); // create an input and it going to update the input, and passing intialState from the user based on the question to usestate
+  // create an input and it going to update the input, and passing intialState from the user based on the question to usestate
+  const [inputs, setInputs] = useState(initialState); 
   const [question, setQuestion] = useState(0); // set the intial question at 0 which will be the first question. it just tracks the question 
 
    const handleOption = (value) => {
@@ -59,9 +59,9 @@ export default function AiRecommendation() {
   // handle the next button by increasing the question by 1
   const handleNextQuestion = () => {
 
-    if (question < questions.length - 1) {
-
-      setQuestion(question + 1 )    
+    if (question < questions.length-1) {
+      setQuestion(question + 1 )
+      
     } else {
       console.log(inputs)
     }
@@ -83,30 +83,22 @@ export default function AiRecommendation() {
      shadow-2xl border-gray-600 py-10 
      px-8 m-4 flex flex-col  items-center min-h-[500px]"
     >
-      <h2 
-       className="text-white text-3xl 
-       font-black mb-8 text-center tracking-tight 
-       drop-shadow-lg"
-      >
-      AI Movie Recommendation
+      <h2 className="text-white text-3xl font-black mb-8 text-center tracking-tight drop-shadow-lg">AI Movie Recommendation
       </h2>
 
       <div className="w-full flex items-center mb-8">
-          <div className=" flex-1 bg-gray-600 rounded-full overflow-hidden h-2">
-           <div className="h-full bg-gray-600 transition-all duration-300" ></div>
-          </div>         
+         <div className=" flex-1 bg-gray-600 rounded-full overflow-hidden h-2">
+          <div className="h-full bg-red-500 transition-all duration-300" style={{ width: `${((question + 1) / questions.length) * 100}%`}}></div>
+         </div>
+        {/* claculating the number of question over lenght of question so that we can get fraction */}
+         <span className="ml-3 text-white text-sm font-semibold">{question + 1}/{questions.length}</span>
       </div>
 
       <div className="w-full flex flex-col flex-1">
          <div className="mb-6 flex-1">
 
          {/* diplay the interation of the lables in the questions array */}
-          <h3 
-           className="text-lg font-semibold
-           text-white text-center mb-5 "
-          > 
-           {questions[question].label}
-          </h3>
+          <h3 className="text-lg font-semibold text-white text-center mb-5 ">{questions[question].label}</h3>
 
           <div className="grid grid-cols-1 gap-2.5">
             {/* map through the options and display each button with a single option*/}
@@ -126,16 +118,15 @@ export default function AiRecommendation() {
           </div>       
          </div>
 
-           <div className=" flex justify-between items-center my-5">
+           <div 
+             className=" flex justify-between items-center my-5"
+            >
             <button 
              onClick={handleBackQuestion}
              type="button" 
-             className="px-4 py-2 rounded-2xl 
-             font-semibold border-2  transition
-             text-white bg-red-600 hover:bg-red-700 cursor-pointer"
-            > 
-             Back
-            </button>
+             className="px-4 py-2 rounded-2xl font-semibold border-2 
+             transition text-white bg-red-600 hover:bg-red-700 cursor-pointer"
+            >Back</button>
 
             <button 
              onClick={()=> handleNextQuestion()}
